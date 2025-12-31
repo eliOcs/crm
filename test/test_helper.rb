6e5f92a4ec1a237fe1a4a8fb499c5042c238d412
Elio Capella <me@eliocapella.com>
2025-12-31 19:48:46 +0100

    Fix CI tests by setting dummy Microsoft OAuth credentials
    Tests were failing in CI because MICROSOFT_CLIENT_ID and
    MICROSOFT_CLIENT_SECRET environment variables weren't set.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,11 @@ require "simplecov"
 SimpleCov.start "rails"
 
 ENV["RAILS_ENV"] ||= "test"
+
+# Set dummy Microsoft OAuth credentials for tests (if not already set)
+ENV["MICROSOFT_CLIENT_ID"] ||= "test-client-id"
+ENV["MICROSOFT_CLIENT_SECRET"] ||= "test-client-secret"
+
 require_relative "../config/environment"
 require "rails/test_help"
 require_relative "test_helpers/session_test_helper"
